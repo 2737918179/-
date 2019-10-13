9b98d5511fa6b3dd65058cb3b4059158d8349e65
2737918179 <2737918179@qq.com>
2019-10-13 22:10:23 +0800

    第五次提交

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 #gmall-user用户服务8080
 gmall-user-service用户服务8070
 gmall-user-web用户服务8080
+gmall-manage-service用户服务8081
+gmall-manage-web用户服务8071
---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/GamallManageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamallManageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamallManageServiceApplication.class, args);
+    }
+
+}
