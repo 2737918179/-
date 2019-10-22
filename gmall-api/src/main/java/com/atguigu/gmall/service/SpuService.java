8080555553c6d8783609004b91d578057d1a7ed1
2737918179 <2737918179@qq.com>
2019-10-22 23:18:05 +0800

    第十四次

--- gmall-api/src/main/java/com/atguigu/gmall/service/SpuService.java
+++ gmall-api/src/main/java/com/atguigu/gmall/service/SpuService.java
@@ -15,5 +15,5 @@ public interface SpuService {
 
     List<PmsProductImage> spuImageList(String spuId);
 
-
+    List<PmsProductSaleAttr> spuSaleAttrListCheckBySku(String productId);
 }
--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SpuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SpuServiceImpl.java
@@ -32,7 +32,6 @@ public class SpuServiceImpl implements SpuService {
     @Override
     public List<PmsProductInfo> spuList(String catalog3Id) {
 
-
         PmsProductInfo pmsProductInfo = new PmsProductInfo();
         pmsProductInfo.setCatalog3Id(catalog3Id);
         List<PmsProductInfo> pmsProductInfos = pmsProductInfoMapper.select(pmsProductInfo);
@@ -64,24 +63,44 @@ public class SpuServiceImpl implements SpuService {
     @Override
     public List<PmsProductSaleAttr> spuSaleAttrList(String spuId) {
 
-       PmsProductSaleAttr pmsProductSaleAttr=new PmsProductSaleAttr();
-       pmsProductSaleAttr.setProductId(spuId);
-       List<PmsProductSaleAttr> pmsProductSaleAttrs=pmsProductSaleAttrMapper.select(pmsProductSaleAttr);
-       for (PmsProductSaleAttr productSaleAttr : pmsProductSaleAttrs){
-           PmsProductSaleAttrValue pmsProductSaleAttrValue=new PmsProductSaleAttrValue();
-           pmsProductSaleAttrValue.setProductId(spuId);
-           pmsProductSaleAttrValue.setSaleAttrId(productSaleAttr.getSaleAttrId());
-           List<PmsProductSaleAttrValue> pmsProductSaleAttrValues = pmsProductSaleAttrValueMapper.select(pmsProductSaleAttrValue);
-           productSaleAttr.setSpuSaleAttrValueList(pmsProductSaleAttrValues);
-       }
+        PmsProductSaleAttr pmsProductSaleAttr = new PmsProductSaleAttr();
+        pmsProductSaleAttr.setProductId(spuId);
+        List<PmsProductSaleAttr> pmsProductSaleAttrs = pmsProductSaleAttrMapper.select(pmsProductSaleAttr);
+        for (PmsProductSaleAttr productSaleAttr : pmsProductSaleAttrs) {
+            PmsProductSaleAttrValue pmsProductSaleAttrValue = new PmsProductSaleAttrValue();
+            pmsProductSaleAttrValue.setProductId(spuId);
+            pmsProductSaleAttrValue.setSaleAttrId(productSaleAttr.getSaleAttrId());
+            List<PmsProductSaleAttrValue> pmsProductSaleAttrValues = pmsProductSaleAttrValueMapper.select(pmsProductSaleAttrValue);
+            productSaleAttr.setSpuSaleAttrValueList(pmsProductSaleAttrValues);
+        }
         return pmsProductSaleAttrs;
     }
 
     @Override
     public List<PmsProductImage> spuImageList(String spuId) {
-        PmsProductImage pmsProductImage=new PmsProductImage();
+        PmsProductImage pmsProductImage = new PmsProductImage();
         pmsProductImage.setProductId(spuId);
-        List<PmsProductImage> pmsProductImages=pmsProductImageMapper.select(pmsProductImage);
+        List<PmsProductImage> pmsProductImages = pmsProductImageMapper.select(pmsProductImage);
         return pmsProductImages;
+    }
+
+    @Override
+    public List<PmsProductSaleAttr> spuSaleAttrListCheckBySku(String productId) {
+        PmsProductSaleAttr pmsProductSaleAttr = new PmsProductSaleAttr();
+        pmsProductSaleAttr.setProductId(productId);
+        List<PmsProductSaleAttr> pmsProductSaleAttrs = pmsProductSaleAttrMapper.select(pmsProductSaleAttr);
+
+        for (PmsProductSaleAttr productSaleAttr : pmsProductSaleAttrs) {
+            String saleAttrId = productSaleAttr.getSaleAttrId();
+
+            PmsProductSaleAttrValue pmsProductSaleAttrValue = new PmsProductSaleAttrValue();
+            pmsProductSaleAttrValue.setSaleAttrId(saleAttrId);
+            pmsProductSaleAttrValue.setProductId(productId);
+            List<PmsProductSaleAttrValue> pmsProductSaleAttrValues = pmsProductSaleAttrValueMapper.select(pmsProductSaleAttrValue);
+
+            productSaleAttr.setSpuSaleAttrValueList(pmsProductSaleAttrValues);
+
+        }
+        return pmsProductSaleAttrs;
     }
 }
